874d478ff132c2b4001de978564d15955755bfcc
mertljan <jm.ostrava@gmail.com>
2021-06-29 02:24:39 +0200

    send

--- eng_proj1.py
+++ eng_proj1.py
@@ -1,6 +1,8 @@
 '''
 author = 
 '''
+import string
+
 TEXTS = ['''
 Situated about 10 miles west of Kemmerer, 
 Fossil Butte is a ruggedly impressive 
@@ -11,7 +13,7 @@ above sea level. The butte is located just
 north of US 30N and the Union Pacific Railroad, 
 which traverse the valley. ''',
 
-         '''At the base of Fossil Butte are the bright 
+'''At the base of Fossil Butte are the bright 
 red, purple, yellow and gray beds of the Wasatch 
 Formation. Eroded portions of these horizontal 
 beds slope gradually upward from the valley floor 
@@ -20,7 +22,7 @@ to the top of the butte are the much steeper
 buff-to-white beds of the Green River Formation, 
 which are about 300 feet thick.''',
 
-         '''The monument contains 8198 acres and protects 
+'''The monument contains 8198 acres and protects 
 a portion of the largest deposit of freshwater fish 
 fossils in the world. The richest fossil fish deposits 
 are found in multiple limestone layers, which lie some 
@@ -28,9 +30,7 @@ are found in multiple limestone layers, which lie some
 represent several varieties of perch, as well as 
 other freshwater genera and herring similar to those 
 in modern oceans. Other fish such as paddlefish, 
-garpike and stingray are also present.
-'''
-         ]
+garpike and stingray are also present.''']
 
 # Vyžádá si od uživatele přihlašovací jméno a heslo.
 # Zjistí, jestli zadané údaje odpovídají někomu z registrovaných uživatelů.
@@ -62,7 +62,18 @@ else:
 
 print("-" * 50)
 txt_cs_in = input("Vyber si číslo textu[1-3] ")
-txt_cs = int(txt_cs_in) - 1
+
+if not txt_cs_in.isdigit():
+    print("Nejedná se o číslo")
+    exit(-1)
+else:
+    if int(txt_cs_in) >= 1 and int(txt_cs_in) <= 3:
+        txt_cs = int(txt_cs_in) - 1
+
+    else:
+        print("Zadané číslo je mimo rozsah")
+        exit(-1)
+
 print("-" * 50)
 txt_anal = TEXTS[txt_cs].split()
 lst_anal = list()
@@ -72,26 +83,28 @@ pct_mala = 0
 pct_cislice = 0
 lst_cislic = list()
 
+char = " ?.!/;:,"
 for wrd in txt_anal:
-    wrd_cl = wrd.strip(".")
-    lst_anal.append(wrd_cl)
+        wrd_cl = wrd.strip(char)
+        lst_anal.append(wrd_cl)
 
+print(lst_anal)
 for i in range(0,len(lst_anal)):
-    if lst_anal[i].istitle():
-        pct_prvni_velke += 1
-    if lst_anal[i].isalpha() and lst_anal[i].isupper():
-        pct_velka += 1
-    if lst_anal[i].isalpha() and lst_anal[i].islower():
-        #print("mala: ",lst_anal[i])
-        pct_mala += 1
-    if lst_anal[i].isdigit():
-        pct_cislice += 1
-        lst_cislic.append(int(lst_anal[i]))
+        if lst_anal[i].istitle():
+            pct_prvni_velke += 1
+        if lst_anal[i].isalpha() and lst_anal[i].isupper():
+            pct_velka += 1
+        if lst_anal[i].isalpha() and lst_anal[i].islower():
+            pct_mala += 1
+
+        if lst_anal[i].isdigit():
+            pct_cislice += 1
+            lst_cislic.append(int(lst_anal[i]))
 
 print(f"There are {len(lst_anal)} words in the selected text")
 print(f"There are {pct_prvni_velke} titlecase words.")
 print(f"There are {pct_velka} uppercase words.")
-print(f"There are {pct_mala} uppercase words.")
+print(f"There are {pct_mala} lowercase words.")
 print(f"There are {pct_cislice} numeric strings.")
 print("The sum of all the numbers: ", sum(lst_cislic))
 
